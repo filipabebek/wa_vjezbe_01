16c5fa1acaa5ef19e045474d1b55fe60e8961c70
Filipa Bebek, Boris Vujica <filipa3986@gmail.com>
2024-10-28 19:46:41 +0100

    Samostalni zadatak za vježbu 1

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
-const PORT = 3000;
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/public/about.html");
+});
+
+app.get("/users", (req, res) => {
+    const users = [
+        { id: 1, ime: "Filipa", prezime: "Bebek" },
+        { id: 2, ime: "Boris", prezime: "Vujica" },
+        { id: 3, ime: "Pero", prezime: "Pero" }
+    ];
+    res.json(users);
+});
+
+const PORT = 3002;
 app.listen(PORT, (error) => {
     if (error) {
         console.log('Greška prilikom pokretanja poslužitelja: ${error.message}');
